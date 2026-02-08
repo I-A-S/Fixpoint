1488a5d72beee60bf3a2b598f33dd0b9ac719c36
dev0 <dev0@iasoft.dev>
2026-02-08 13:46:02 +0530

    Init

---
+++ README.md
@@ -0,0 +1,21 @@
+<div align="center">
+  <img src="logo.png" alt="Fixpoint Logo" width="190" style="border-radius: 1.15rem;"/>
+  <br/>
+  
+  <img src="https://img.shields.io/badge/license-apache_v2-blue.svg" alt="License"/>
+  <img src="https://img.shields.io/badge/standard-C%2B%2B20-yellow.svg" alt="C++ Standard"/>
+
+  <p style="padding-top: 0.2rem;">
+    <b>Powerful static analysis, simplified.</b>
+  </p>
+</div>
+
+## **Description**
+
+## **Features**
+
+## **Quick Start**
+
+## **License**
+
+Copyright (C) 2026 IAS. Licensed under the [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0).
---
+++ cmake/find_deps.cmake
@@ -0,0 +1,40 @@
+include(FetchContent)
+
+if(MSVC)
+    set(Clang_DIR "$ENV{WIN_LLVM_DIR}/lib/cmake/clang")
+    set(LLVM_DIR "$ENV{WIN_LLVM_DIR}/lib/cmake/llvm")
+endif()
+
+find_package(Clang REQUIRED CONFIG)
+find_package(LLVM REQUIRED CONFIG)
+
+if(MSVC)
+    set(LLVM_TARGETS_TO_SCRUB ${LLVM_AVAILABLE_LIBS} LLVMDebugInfoCodeView LLVMDebugInfoPDB LLVMDebugInfoMSF)
+
+    foreach(TGT ${LLVM_TARGETS_TO_SCRUB})
+        if(TARGET ${TGT})
+            get_target_property(DEPS ${TGT} INTERFACE_LINK_LIBRARIES)
+            if(DEPS)
+                list(FILTER DEPS EXCLUDE REGEX ".*diaguids.*")
+                set_target_properties(${TGT} PROPERTIES INTERFACE_LINK_LIBRARIES "${DEPS}")
+            endif()
+    
+            get_target_property(DEPS_DEBUG ${TGT} IMPORTED_LINK_INTERFACE_LIBRARIES_DEBUG)
+            if(DEPS_DEBUG)
+                list(FILTER DEPS_DEBUG EXCLUDE REGEX ".*diaguids.*")
+                set_target_properties(${TGT} PROPERTIES IMPORTED_LINK_INTERFACE_LIBRARIES_DEBUG "${DEPS_DEBUG}")
+            endif()
+        endif()
+    endforeach()
+    
+    message(STATUS "LLVM Targets scrubbed of absolute diaguids.lib paths.")
+endif()
+
+FetchContent_Declare(
+    IATest
+    GIT_REPOSITORY https://github.com/I-A-S/IATest
+    GIT_TAG        main
+    OVERRIDE_FIND_PACKAGE
+)
+
+FetchContent_MakeAvailable(IATest)
